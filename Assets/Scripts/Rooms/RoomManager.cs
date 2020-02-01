174af95a28914069c00360ba745c73581a6c70d1
Paul Pop <pauldarius98@gmail.com>
2020-02-01 13:13:05 +0200

    chore: Add some material files

--- Assets/Scripts/Rooms/RoomManager.cs
+++ Assets/Scripts/Rooms/RoomManager.cs
@@ -7,16 +7,16 @@ namespace GGJ.Rooms
 {
 	public class RoomManager : MonoBehaviour
 	{
-		[SerializeField] private Room roomPrefab;
-		[SerializeField] private Entrance entrancePrefab;
-
+		private static int roomNumber = 1;
 		public Room CurrentRoom { get; private set; }
 		public Room PreviouseRoom { get; private set; }
 		public Action<Room, Room> OnRoomEntered { get; private set; }
-
 		public Transform EntrancesParent { get; private set; }
-
 		private HashSet<Entrance> entrances = new HashSet<Entrance>();
+
+		private Room roomPrefab;
+		private Entrance entrancePrefab;
+		private Transform roomsParent;
 
 		public List<Entrance> GetEntrancesForRoom(Room room)
 		{
@@ -29,7 +29,8 @@ namespace GGJ.Rooms
 		public Room GenerateRoom(Vector3 entrancePosition, Entrance entrance, Direction direction)
 		{
 			var position = entrancePosition + Utils.GetVectorDirection(direction) * 6f - Vector3.up * 0.5f; /*calculate this based on the size of the rooms*/
-			var newRoom = Instantiate(roomPrefab, position, Quaternion.identity);
+			var newRoom = Instantiate(roomPrefab, position, Quaternion.identity, roomsParent);
+			newRoom.name = "Room " + roomNumber++.ToString();
 			entrance.ConnectedRooms.Item2 = newRoom;
 			return newRoom;
 		}
@@ -79,6 +80,7 @@ namespace GGJ.Rooms
 			EntrancesParent = new GameObject("Entrances").transform;
 			roomPrefab = Resources.Load<Room>(Paths.PREFABS + "Room");
 			entrancePrefab = Resources.Load<Entrance>(Paths.PREFABS + "Entrance");
+			roomsParent = GameObject.Find("Rooms").transform ?? new GameObject("Rooms").transform;
 		}
 	}
 }
