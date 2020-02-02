8160e8299c061bcddba059f24a3dfe41b1a41bd8
GyalayPaul <gyalay.paul@yahoo.com>
2020-02-02 14:42:04 +0200

    wip: more work

--- Assets/Scripts/Rooms/Console.cs
+++ Assets/Scripts/Rooms/Console.cs
@@ -1,4 +1,5 @@
 ﻿using GGJ.Rooms;
+using UnityEngine;
 
 namespace GGJ
 {
@@ -9,6 +10,9 @@ namespace GGJ
 		public Room Room;
 
 		private string puzzleId = "";
+
+		public Light Light;
+		public Renderer Monitor;
 
 		public void SetConsoleId(string id)
 		{
@@ -33,6 +37,28 @@ namespace GGJ
 		public void SetConsoleState(ConsoleState newState)
 		{
 			ConsoleState = newState;
+			switch (newState)
+			{
+				case ConsoleState.Locked:
+					Monitor.materials[1] = Resources.Load<Material>(Constants.StationMaterialPath);
+					Light.intensity = 0;
+					break;
+
+				case ConsoleState.Interactable:
+					Monitor.materials[1] = Monitor.materials[1] = Resources.Load<Material>(Constants.BlueEmissionMaterialPath);
+					Light.color = new Color(Constants.BlueLightColorColor.X, Constants.BlueLightColorColor.Y, Constants.BlueLightColorColor.Z);
+					Light.intensity = 1;
+					break;
+
+				case ConsoleState.Resolved:
+					Monitor.materials[1] = Monitor.materials[1] = Resources.Load<Material>(Constants.GreenEmissionMaterialPath);
+					Light.color = new Color(Constants.GreenLightColor.X, Constants.GreenLightColor.Y, Constants.GreenLightColor.Z);
+					Light.intensity = 1;
+					break;
+
+				default:
+					break;
+			}
 		}
 	}
 }
--- Assets/Scripts/Utils/Constants.cs
+++ Assets/Scripts/Utils/Constants.cs
@@ -1,8 +1,26 @@
-﻿public static class Constants
+﻿using System.Numerics;
+
+public static class Constants
 {
-    public const string SIMON_ID = "simon";
-    public const string REACTION_SPEED_ID = "reactionSpeed";
-    public const string MEMORY_ID = "memory";
-    public const string JIGSAW_ID = "jigsaw";
-    public const string KEYSEQUENCE_ID = "keysequnce";
+	public const string SIMON_ID = "simon";
+	public const string REACTION_SPEED_ID = "reactionSpeed";
+	public const string MEMORY_ID = "memory";
+	public const string JIGSAW_ID = "jigsaw";
+	public const string KEYSEQUENCE_ID = "keysequnce";
+
+	public static Vector3 BlueLightColorColor => new Vector3(0.8196079f, 1f, 1f);
+
+	public static Vector3 GreenLightColor => new Vector3(0.72f, 1f, 0.85f);
+
+	public static Vector3 RedLightColor => new Vector3(1f, 0.5f, 0.5f);
+
+	public const string GreenEmissionMaterialPath = "Materials/Emissive - Green";
+
+	public const string OrangeEmissionMaterialPath = "Materials/Emissive - Orange";
+
+	public const string BlueEmissionMaterialPath = "Materials/Emissive - Blue";
+
+	public const string RedEmissionMaterialPath = "Materials/Emissive - Red";
+
+	public const string StationMaterialPath = "Materials/StationBase";
 }
